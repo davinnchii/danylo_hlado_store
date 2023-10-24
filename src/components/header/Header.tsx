5d7ccf55d6d1dda763fd32631372bdf58f6372eb
daviinnchi <zaikoivan99@gmail.com>
2023-10-24 12:21:06 +0200

    Fix linter and merge problems in all files

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,42 +1,45 @@
-import { useState } from 'react';
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import React, { useState } from 'react';
 
 // import logo from '../../assets/images/logo.png';
 // import second from '../../assets/images/logo_black.png';
 import './header.scss';
 
-
-export const Header = () => {
+export const Header: React.FC = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
     <header className="header">
       <div className="header__left">
-        <a className="header__logo" href='/'>
+        <a className="header__logo" href="/">
           <img
-            className='header__logo-img'
+            className="header__logo-img"
             src={isMenuOpen ? 'logo' : 'logo_black'}
             alt="logo"
           />
         </a>
 
         <nav className={`header__menu ${isMenuOpen ? 'active-menu' : ''}`}>
-          <a href="/home" className="header__menu-link active-menu-link">home</a>
+          <a href="/home" className="header__menu-link active-menu-link">
+            home
+          </a>
           <a href="/phones" className="header__menu-link">phones</a>
           <a href="/tablets" className="header__menu-link">tablets</a>
           <a href="/accessories" className="header__menu-link">accessories</a>
         </nav>
       </div>
 
-    <div className="header__right">
-      <div className="header__icons">
-        <i
-          className={`icon icon--${!isMenuOpen ? 'menu' : 'close'}`}
-          onClick={() => setIsMenuOpen(prev => !prev)}
-        />
-        <i className="icon icon--favourites" />
-        <i className="icon icon--shopping-bag" />
+      <div className="header__right">
+        <div className="header__icons">
+          <i
+            className={`icon icon--${!isMenuOpen ? 'menu' : 'close'}`}
+            onClick={() => setIsMenuOpen(prev => !prev)}
+          />
+          <i className="icon icon--favourites" />
+          <i className="icon icon--shopping-bag" />
+        </div>
       </div>
-    </div>
-  </header>
-  )
-}
+    </header>
+  );
+};
