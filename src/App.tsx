e9d6fab808746545290e677b1433d541e664165d
Taras Voievoda <voevoda.taras@gmail.com>
2023-10-25 17:09:54 +0300

    Fix conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,17 @@
 import React from 'react';
 
-import { Header } from './components/header/Header';
-
+import { Outlet } from 'react-router-dom';
+import { Header } from './components/Header/Header';
 import { Footer } from './components/footer/Footer';
-// import { HomePage } from './pages/HomePage/HomePage';
-import { ItemCard } from './components/ItemCard';
+
 import './App.scss';
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
+    <>
       <Header />
-      <ItemCard />
+      <Outlet />
       <Footer />
-    </div>
+    </>
   );
-}
-
-export default App;
+};
