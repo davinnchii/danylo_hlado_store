41fc0ef09abd6980c23c0a9b918b74f3d53758ef
Taras Voievoda <voevoda.taras@gmail.com>
2023-10-25 17:58:11 +0300

    changed image error path

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+
+import erorr_404 from '../../assets/images/404-error.png';
 import './NotFoundPage.scss';
 
 export const NotFoundPage: React.FC = () => (
   <div className="page-not-found">
     <h1 className="title">Page not found</h1>
     <img
-      src="../../assets/images/404-image.png"
+      src={erorr_404}
       alt="error 404"
       className="page-not-found_img"
     />
