8379939af877c35c722aa07d724dad459f482a34
davinnchii <zaikoivan99@gmail.com>
2023-10-26 17:58:04 +0200

    Add item card page

--- src/Root.tsx
+++ src/Root.tsx
@@ -6,7 +6,7 @@ import {
   Routes,
 } from 'react-router-dom';
 
-import { App } from './App';
+import { App } from './App'
 import { HomePage } from './pages/HomePage/HomePage';
 import { NotFoundPage } from './pages/NotFoundPage/NotFoundPage';
 
