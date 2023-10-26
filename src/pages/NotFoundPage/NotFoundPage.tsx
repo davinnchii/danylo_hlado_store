6efa3e3122289b8925b7718b7a2b197047ec0096
Taras Voievoda <voevoda.taras@gmail.com>
2023-10-26 12:02:55 +0300

    changed router path, variable name

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import erorr_404 from '../../assets/images/404-error.png';
+import ERROR_NOT_FOUND from '../../assets/images/404-error.png';
 import './NotFoundPage.scss';
 
 export const NotFoundPage: React.FC = () => (
   <div className="page-not-found">
     <h1 className="title">Page not found</h1>
     <img
-      src={erorr_404}
+      src={ERROR_NOT_FOUND}
       alt="error 404"
       className="page-not-found_img"
     />
