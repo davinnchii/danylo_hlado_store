8ae037f54d0eec0c71c18273d3be0ce9dd747d96
Ivan Zayko <128422319+davinnchii@users.noreply.github.com>
2023-10-26 11:19:01 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { Outlet } from 'react-router-dom';
-import { Header } from './components/Header/Header';
+import { Header } from './components/header/Header';
 import { Footer } from './components/footer/Footer';
 
 import './App.scss';
