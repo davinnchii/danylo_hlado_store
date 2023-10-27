75a2a1c001492eed582ce54e571fae0618911564
Nikita Zaika <zaikanikitaua@gmail.com>
2023-10-27 19:27:51 +0300

    fix-title-card-bug-and-card-width-bag

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import './App.scss';
 
+import { Outlet } from 'react-router-dom';
 import { Header } from './components/header/Header';
 import { Footer } from './components/footer/Footer';
 
