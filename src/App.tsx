09274472138ea0bebd3eec3e9330a0d761e51035
davinnchii <zaikoivan99@gmail.com>
2023-10-28 15:13:36 +0200

    fix merge conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 
 import './App.scss';
 
-import { Header } from './components/Header/Header';
+import { Header } from './components/Header';
 import { Footer } from './components/footer/Footer';
 
 export const App = () => {
