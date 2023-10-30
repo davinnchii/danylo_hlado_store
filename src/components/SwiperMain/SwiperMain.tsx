67193551c1e5c882f5fd9df5b69f23b4a18f17b2
Taras Voievoda <voevoda.taras@gmail.com>
2023-10-30 13:53:45 +0200

    added banners for main-swiper, added order button

--- src/components/SwiperMain/SwiperMain.tsx
+++ src/components/SwiperMain/SwiperMain.tsx
@@ -2,9 +2,16 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+import { Link, useSearchParams } from 'react-router-dom';
 
-import banner from '../../assets/images/banner.png';
-import bannerMobile from '../../assets/images/banner-mobile.png';
+import bannerPhone from '../../assets/images/banner-phone.png';
+import bannerWatch from '../../assets/images/banner-watch.png';
+import bannerIpad from '../../assets/images/banner-ipad.png';
+
+import bannerPhoneMobile from '../../assets/images/banner-phone-mobile.png';
+import bannerWatchMobile from '../../assets/images/banner-watch-mobile.png';
+import bannerIpadMobile from '../../assets/images/banner-ipad-mobile.png';
+
 import arrowLeftBlack from '../../assets/icons/arrow-left-black.svg';
 import arrowRightBlack from '../../assets/icons/arrow-right-black.svg';
 
@@ -12,31 +19,55 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import './swiperMain.scss';
+import { limitDefault, offsetDefault, sortDefault } from '../../utils/constant';
+import { normalizedMenuLink } from '../../utils/getNormalizedMenuLink';
+
+const banners = [
+  {
+    id: 'phones',
+    imgUrl: bannerPhone,
+    imgUrlMobile: bannerPhoneMobile,
+  },
+  {
+    id: 'accessories',
+    imgUrl: bannerWatch,
+    imgUrlMobile: bannerWatchMobile,
+  },
+  {
+    id: 'tablets',
+    imgUrl: bannerIpad,
+    imgUrlMobile: bannerIpadMobile,
+  },
+];
 
 export const SwiperMain = () => {
+  const [searchParams] = useSearchParams();
+  const params = new URLSearchParams(searchParams);
+  const sort = params.get('sortBy') || sortDefault;
+
   return (
     <Swiper
       modules={[Navigation, Pagination, Autoplay]}
       className="mySwiper"
       autoplay={{
-        delay: 2500,
+        delay: 3500,
         disableOnInteraction: false,
       }}
       pagination={{ clickable: true }}
       navigation={{ nextEl: '.swiper-slide__main-arrow-right', prevEl: '.swiper-slide__main-arrow-left' }}
     >
-      {[1, 2, 3].map(item => (
-        <SwiperSlide key={item}>
+      {banners.map(item => (
+        <SwiperSlide key={item.id}>
           <div className="swiper-slide__img-box">
             <img
               className="swiper-slide__img"
-              src={banner}
+              src={item.imgUrl}
               alt="banner"
             />
 
             <img
               className="swiper-slide__img-mobile"
-              src={bannerMobile}
+              src={item.imgUrlMobile}
               alt="banner"
             />
 
@@ -56,6 +87,18 @@ export const SwiperMain = () => {
               <img src={arrowRightBlack} alt="arrow_right_black" />
             </button>
           </div>
+
+          <Link
+            className="swiper-slide__main-order-link"
+            to={normalizedMenuLink(item.id, limitDefault, offsetDefault, sort)}
+          >
+            <button
+              type="button"
+              className="swiper-slide__main-order-button"
+            >
+              Order now
+            </button>
+          </Link>
         </SwiperSlide>
       ))}
     </Swiper>
