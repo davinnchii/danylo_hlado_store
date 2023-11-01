e73baaca98628d5af90d91a178f3b362cc0f9b65
davinnchii <zaikoivan99@gmail.com>
2023-11-01 21:03:59 +0100

    Fix pagination bugs

--- src/pages/PhonesPage/PhonesPage.tsx
+++ src/pages/PhonesPage/PhonesPage.tsx
@@ -57,9 +57,13 @@ export const PhonesPage: React.FC = () => {
   }, [category, sort, limit, offset, updateRequest, query]);
 
   const handleChangePage = useCallback((page: number) => {
+    const params = new URLSearchParams(searchParams);
+
     setCurrentPage(page);
-    searchParams.set('offset', (Number(limit) * page - 1).toString());
-  }, [searchParams, limit]);
+    params.set('offset', (Number(limit) * (page - 1)).toString());
+    params.set('page', page.toString());
+    setSearchParams(params);
+  }, [limit, sort]);
 
   const handleChangeParams = useCallback((event: SelectChangeEvent) => {
     const params = new URLSearchParams(searchParams);
